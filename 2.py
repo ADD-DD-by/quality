1737238eef971873dc4d4721b160ceb04ede34d6
ADD-DD-by <1344109089@qq.com>
2026-03-20 08:53:04 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
-
+# 版本：体积 + 重量 + 重量/围长
 # =========================
 # 基础函数
 # =========================
