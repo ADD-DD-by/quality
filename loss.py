ef53e684abce523995a09373a00c2ed2ae2bdc9b
ADD-DD-by <1344109089@qq.com>
2026-03-21 09:58:45 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -115,7 +115,7 @@ def predict_row(row, coef):
 # 3️⃣ 单条预测
 # =========================
 st.subheader("🔹 单条预测")
-
+st.subheader("🔹 上传数据表列名：包装长、包装宽、包装高、包装重、围长、包装系数")
 col1, col2, col3, col4 = st.columns(4)
 
 L = col1.number_input("包装长", value=120.0)
